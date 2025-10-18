7a98d8168b7020e510ac93e2debaa130997c273e
Michael Tio <77330912+michaeltio@users.noreply.github.com>
2025-10-18 15:36:06 +0700

    ganti image

--- database/models/invoice_model.go
+++ database/models/invoice_model.go
@@ -32,6 +32,16 @@ type InvoiceDetail struct {
 	DeletedAt        time.Time
 }
 
+
+type InvoiceResponse struct {
+	InvoiceID     string `json:"invoice_id"`
+	IssueDate     time.Time `json:"issue_date"`
+	DueDate       time.Time `json:"due_date"`
+	Total         float64 `json:"total"`
+	AmountPaid    float64 `json:"amount_paid"`
+	PaymentStatus string `json:"payment_status"`
+}
+
 type InvoiceExtendedResponse struct {
 	InvoiceID        string `json:"invoice_id"`
 	InvoiceNumber    string `json:"invoice_number"`
@@ -45,15 +55,6 @@ type InvoiceExtendedResponse struct {
 	AmountPaid       float64   `json:"amount_paid"`
 	PaymentStatus    string    `json:"payment_status"`
 	VoidedAt         time.Time `json:"voided_at"`
-}
-
-type InvoiceResponse struct {
-	InvoiceID     string `json:"invoice_id"`
-	IssueDate     time.Time `json:"issue_date"`
-	DueDate       time.Time `json:"due_date"`
-	Total         float64 `json:"total"`
-	AmountPaid    float64 `json:"amount_paid"`
-	PaymentStatus string `json:"payment_status"`
 }
 
 type InvoiceDetailResponse struct {
